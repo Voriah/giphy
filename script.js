26600da2169d44c9d5c42b6c2f0551c538910123
Voriah <rodgick@yahoo.com>
2018-09-11 23:41:57 -0700

    caroosels

--- script.js
+++ script.js
@@ -1,7 +1,52 @@
+var topics = ["overwatch", "destiny", "warcraft", "fails", "wins", "cats", "wat"];
+
+$(document).ready(function () {
+  $('.carousel').carousel();
+  $('.carousel-slider').slider({ full_width: true });
+
+  for (i = 0; i < topics.length; i++) {
+    $(".buttons").append(`
+  <a class="searchBtn waves-effect waves-light btn">${topics[i]}</a>
+  `);
+  }
+});
+
+var carnum = 0;
 
 function doit() {
+  event.preventDefault();
   var query = $("#search").val();
-console.log(query);
-  var url = $.get(`https://api.giphy.com/v1/gifs/search?q=${query}&api_key=GlK3OuB6tPyAXkwD6jclG9HFFVuCHlFa`);
-  url.then (function (data) { console.log("success got data", data); });
+  $(".buttons").append(`
+  <a class="searchBtn waves-effect waves-light btn">${query}</a>
+  `);
+  $("#search").val("")
 }
+
+$(document).on("click", ".searchBtn", function () {
+  var query = $(this).text();
+  console.log(query)
+  var url = $.get(`https://api.giphy.com/v1/gifs/search?q=${query}&api_key=GlK3OuB6tPyAXkwD6jclG9HFFVuCHlFa`)
+  .then (function (response) { 
+    console.log(response); 
+    var ids = "car" + carnum;
+    $(".gifHolder").append(`<div id="${ids}" class="carousel carousel-slider"></div>`)
+    for (i = 0; i < 10; i++) {
+      $(`#${ids}`).append(`<a class="carousel-item"><img class="gifs" src='${response.data[i].images.original_still.url}' alt="${response.data[i].images.original.url}"></a>`);
+    };
+    carnum++
+
+    setTimeout(function() {
+    $('.carousel').carousel();
+    $('.carousel-slider').slider({ full_width: true });
+    }, 500)
+  }).catch() 
+  
+});
+
+$(document).on("click", ".gifs", function () {
+  var still = $(this).attr("src")
+  var anim = $(this).attr("alt")
+
+  $(this).attr("src", anim)
+  $(this).attr("alt", still)
+});
